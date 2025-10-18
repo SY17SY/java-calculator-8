ff3bd101cc4238c87fb116ae4a6854d3ad0b108f
Seoyeong Kim <rlatjdud6573@naver.com>
2025-10-19 00:54:06 +0900

    fix: 8. 입력값 : 엔터 이용 case 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,6 +6,10 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+        if (input.contains("//") && !input.contains("\\n")) {
+            String body = Console.readLine();
+            input = input + "\\n" + body;
+        }
         int result = StringCalculator.add(input);
         System.out.println("결과 : " + result);
     }
