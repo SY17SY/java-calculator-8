6642acd214be0553e6f60cc89b51c173b5451308
yeong <rlatjdud6573@naver.com>
2025-10-20 23:51:47 +0900

    feat: (InputView) 사용자 입력 요구 메시지 출력/입력 기능

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,4 +1,16 @@
 package calculator.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.io.IOException;
+
 public class InputView {
+    public String input() throws IOException {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String first = Console.readLine();
+        if (first.startsWith("//") && !first.contains("\\n")) {
+            String second = Console.readLine();
+            first = first + "\\n" + second;
+        }
+        return first;
+    }
 }
