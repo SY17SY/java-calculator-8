c3ea36628795f498d9ced33fae10bc085119d3a5
yeong <rlatjdud6573@naver.com>
2025-10-21 00:27:55 +0900

    feat: (Application) 의존성 주입

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.domain.Calculator;
+import calculator.domain.DelimiterGenerator;
+import calculator.domain.NumberSeparator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IOException {
+        DelimiterGenerator delimiterGenerator = new DelimiterGenerator();
+        NumberSeparator numberSeparator = new NumberSeparator(delimiterGenerator);
+        Calculator calculator = new Calculator(numberSeparator);
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        CalculatorController controller = new CalculatorController(inputView, outputView, calculator);
+
+        controller.runCalculator();
     }
 }
