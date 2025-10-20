e0b1e3d29d8e31f77be12aaa83cc81459c0d0728
yeong <rlatjdud6573@naver.com>
2025-10-20 23:50:53 +0900

    feat: CalculatorController 설계

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,24 @@
 package calculator.controller;
 
+import calculator.domain.Calculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+import java.io.IOException;
+
 public class CalculatorController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Calculator calculator;
+
+    public CalculatorController(InputView inputView, OutputView outputView, Calculator calculator) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.calculator = calculator;
+    }
+
+    public void runCalculator() throws IOException {
+        String stringInputs = inputView.input();
+//        int result = calculator.add(stringInputs);
+//        outputView.printResult(result);
+    }
 }
