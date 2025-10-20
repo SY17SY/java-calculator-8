eaf839d6f26d72df9f106c231c5a6773830bf1ed
yeong <rlatjdud6573@naver.com>
2025-10-21 00:21:16 +0900

    feat: (Calculator) int[]의 합 계산 클래스 구현

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,4 +1,18 @@
 package calculator.domain;
 
 public class Calculator {
+    private final NumberSeparator numberSeparator;
+
+    public Calculator(NumberSeparator numberSeparator) {
+        this.numberSeparator = numberSeparator;
+    }
+
+    public int add(String stringInputs) {
+        int[] splitNum = numberSeparator.splitNum(stringInputs);
+        int sum = 0;
+        for (int num : splitNum) {
+            sum += num;
+        }
+        return sum;
+    }
 }
