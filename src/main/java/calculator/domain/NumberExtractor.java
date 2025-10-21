d657f4227913f21b7c6a305e90539fb553c27d7c
yeong <rlatjdud6573@naver.com>
2025-10-21 11:17:45 +0900

    feat: (NumberExtractor) 문자열 분리 후 List<Integer> 반환 클래스 구현

---
+++ src/main/java/calculator/domain/NumberExtractor.java
@@ -0,0 +1,28 @@
+package calculator.domain;
+
+import calculator.dto.SplitStringDto;
+import java.util.Arrays;
+import java.util.List;
+
+public class NumberExtractor {
+    private final DelimiterParser delimiterParser;
+
+    public NumberExtractor(DelimiterParser delimiterParser) {
+        this.delimiterParser = delimiterParser;
+    }
+
+    public List<Integer> extract(String stringInput) {
+        String[] stringInputs = splitString(stringInput);
+        return Arrays.stream(stringInputs)
+                .mapToInt(Integer::parseInt)
+                .boxed().toList();
+    }
+
+    private String[] splitString(String input) {
+        SplitStringDto delimiterAndTarget = delimiterParser.parse(input);
+        String delimiter = delimiterAndTarget.delimiter();
+        String targetString = delimiterAndTarget.targetString();
+
+        return targetString.split(delimiter);
+    }
+}
