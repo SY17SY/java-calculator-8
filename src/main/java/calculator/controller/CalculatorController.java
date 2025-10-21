5391aa5b480a2defd463fb9b6f11d0d45e86e21b
yeong <rlatjdud6573@naver.com>
2025-10-21 10:11:50 +0900

    feat: CalculatorController 생성

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,23 @@
 package calculator.controller;
 
+import calculator.domain.Calculator;
+import calculator.view.ConsoleInput;
+import calculator.view.ConsoleOutput;
+
 public class CalculatorController {
+    private final ConsoleInput consoleInput;
+    private final ConsoleOutput consoleOutput;
+    private final Calculator calculator;
+
+    public CalculatorController(ConsoleInput consoleInput, ConsoleOutput consoleOutput, Calculator calculator) {
+        this.consoleInput = consoleInput;
+        this.consoleOutput = consoleOutput;
+        this.calculator = calculator;
+    }
+
+    public void runCalculator() {
+//        String stringInputs = consoleInput.input();
+//        int result = calculator.add(stringInputs);
+//        consoleOutput.printResult(result);
+    }
 }
