e87fa2772755b29ce549f8fd816e2d08f720ad80
yeong <rlatjdud6573@naver.com>
2025-10-21 11:18:12 +0900

    feat: (Calculator) List<Integer>의 합 계산 클래스 구현

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,4 +1,20 @@
 package calculator.domain;
 
+import java.util.List;
+
 public class Calculator {
+    private final NumberExtractor numberExtractor;
+
+    public Calculator(NumberExtractor numberExtractor) {
+        this.numberExtractor = numberExtractor;
+    }
+
+    public int add(String stringInput) {
+        List<Integer> intInputs = numberExtractor.extract(stringInput);
+        int sum = 0;
+        for (int num : intInputs) {
+            sum += num;
+        }
+        return sum;
+    }
 }
