074f68349497d6bb719694be57d9af308ec4ceda
yeong <rlatjdud6573@naver.com>
2025-10-21 11:18:52 +0900

    feat: (Application) 의존성 주입

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.domain.Calculator;
+import calculator.domain.DelimiterParser;
+import calculator.domain.NumberExtractor;
+import calculator.view.ConsoleInput;
+import calculator.view.ConsoleOutput;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        DelimiterParser delimiterParser = new DelimiterParser();
+        NumberExtractor numberExtractor = new NumberExtractor(delimiterParser);
+        Calculator calculator = new Calculator(numberExtractor);
+
+        ConsoleInput consoleInput = new ConsoleInput();
+        ConsoleOutput consoleOutput = new ConsoleOutput();
+
+        CalculatorController controller = new CalculatorController(consoleInput, consoleOutput, calculator);
+
+        controller.runCalculator();
     }
 }
